69a49a02d09fa1d2e8410d7fea91707826d7fd0c
Hudson SIlva Borges <hudsonsilbor@gmail.com>
2017-07-18 22:31:18 -0300

    Remove tests - Pull Request #16

--- test/comment.js
+++ test/comment.js
@@ -8,30 +8,9 @@ const input = `
 	@COMMENT{"\nslkdjflksdjflkdsjf"}
 
 	% test
-
-	@comment{paglione2001mapping,
-	  title={A Mapping Survey of the 13CO and 12CO Emission in Galaxies},
-	  author={Paglione, T.A.D. and Wall, WF and Young, J.S. and Heyer, M.H. and Richard, M. and Goldstein, M. and Kaufman, Z. and Nantais, J. and Perry, G.},
-	  journal={The Astrophysical Journal Supplement Series},
-	  volume={135},
-	  pages={183}, %test end comment out
-	  year={2001},
-	  %comment out ={}
-	  publisher={IOP % comment middel works  test Publishing}
-	}
 `;
 
 const output = [ { entryType: 'PREAMBLE', entry: '"ewcommand{oopsort}[1]{}"' },
-  { entryType: 'COMMENT', entry: '"slkdjflksdjflkdsjf"' },
-  { citationKey: 'paglione2001mapping',
-    entryType: 'comment',
-    entryTags:
-     { title: 'A Mapping Survey of the 13CO and 12CO Emission in Galaxies',
-       author: 'Paglione, T.A.D. and Wall, WF and Young, J.S. and Heyer, M.H. and Richard, M. and Goldstein, M. and Kaufman, Z. and Nantais, J. and Perry, G.',
-       journal: 'The Astrophysical Journal Supplement Series',
-       volume: '135',
-       pages: '183',
-       year: '2001',
-       publisher: 'IOP % comment middel works  test Publishing' } } ]
+  { entryType: 'COMMENT', entry: '"slkdjflksdjflkdsjf"' } ]
 
-test('Should parse commwnt', t => t.deepEqual(output, bibtexParse.toJSON(input)));
+test('Should parse comment', t => t.deepEqual(output, bibtexParse.toJSON(input)));
