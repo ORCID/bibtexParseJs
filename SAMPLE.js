bbe43a2560fb317f267094ed2cb3838ab55cb90d
Norman Koch <norman.koch@tu-dresden.de>
2023-09-08 14:28:45 +0200

    fixed typo

--- SAMPLE.js
+++ SAMPLE.js
@@ -2,10 +2,12 @@ var bibtexParse = require('bibtex-parse-js');
 
 var sample = bibtexParse.toJSON('@article{sample1,title={sample title}}');
 
-# Will conolse log:
+/*
+# Will console log:
 # 
 # [ { citationKey: 'SAMPLE1',
 #     entryType: 'ARTICLE',
 #     entryTags: { TITLE: 'sample title' } } ]
 #
+*/
 console.log(sample);
